61bb61c0bf2e6fd24621a8d6eb8097f32abe566f
Andrii Poliukh <andrii.poliukh@gmail.com>
2025-09-07 19:43:00 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,115 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import React from 'react';
+import {
+  Routes,
+  Route,
+  Link,
+  useLocation,
+  useParams,
+  Navigate,
+} from 'react-router-dom';
+import { Tab } from './types/Tab';
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+// Навбар з активними посиланнями
+const Navbar: React.FC = () => {
+  const location = useLocation();
+
+  return (
     <nav
       className="navbar is-light is-fixed-top is-mobile has-shadow"
       data-cy="Nav"
     >
       <div className="container">
         <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">
+          <Link
+            to="/"
+            className={`navbar-item ${location.pathname === '/' ? 'is-active' : ''}`}
+          >
             Home
-          </a>
-          <a href="/tabs" className="navbar-item">
+          </Link>
+
+          <Link
+            to="/tabs"
+            className={`navbar-item ${
+              location.pathname.startsWith('/tabs') ? 'is-active' : ''
+            }`}
+          >
             Tabs
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
+  );
+};
 
-    <div className="section">
-      <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
+// Головна сторінка
+const HomePage: React.FC = () => <h1 className="title">Home page</h1>;
 
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
+// Сторінка з табами
+const TabsPage: React.FC = () => {
+  const { tabId } = useParams<{ tabId?: string }>();
+  const activeTab = tabs.find(tab => tab.id === tabId);
 
-        <div className="block" data-cy="TabContent">
-          Please select a tab
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={tab.id === tabId ? 'is-active' : ''}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {tabId
+          ? activeTab
+            ? activeTab.content
+            : 'Please select a tab'
+          : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
+
+// 404 сторінка
+const NotFoundPage: React.FC = () => <h1 className="title">Page not found</h1>;
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="section" style={{ paddingTop: '4.5rem' }}>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+
+            <Route path="/tabs">
+              <Route index element={<TabsPage />} />
+              <Route path=":tabId" element={<TabsPage />} />
+            </Route>
+
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
